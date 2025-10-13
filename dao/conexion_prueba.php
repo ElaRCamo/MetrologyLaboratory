b38cd37a64695cbae5592aea3086fda6cd10d413
ElaRCamo <112036581+ElaRCamo@users.noreply.github.com>
2025-10-12 19:22:24 -0600

    Update conexion_prueba.php

--- dao/conexion_prueba.php
+++ dao/conexion_prueba.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$conexion = mysqli_connect("127.0.0.1:3306","u909553968_Ela","LMGrammer2024#","u909553968_MetroTickects");
+$conexion = mysqli_connect("IP:PUERTO","USER","PASSWORD","ESQUEMA");
 
 if($conexion){
     echo 'Conexión exitosa';
